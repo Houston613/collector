5927912fe4184d9951f7f2c23b222885f5beaecc
Червак Антон <mr.antonchervak@gmail.com>
2026-03-11 10:36:13 +0300

    iter4: Добавлена проверка флагов. Добавлена мьютекс при выполнении методов в репозитории

--- internal/repository/storage.go
+++ internal/repository/storage.go
@@ -1,6 +1,11 @@
 package repository
 
+import "maps"
+
+import "sync"
+
 type StructMem struct {
+	mu       sync.RWMutex
 	gauges   map[string]float64
 	counters map[string]int64
 }
@@ -22,27 +27,45 @@ func NewStructMem() *StructMem {
 }
 
 func (m *StructMem) UpdateGauge(name string, value float64) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	m.gauges[name] = value
 }
 
+//лок на чтение для консистентности 
 func (m *StructMem) UpdateCounter(name string, value int64) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	m.counters[name] += value
 }
 
 func (m *StructMem) GetGauge(name string) (float64, bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
 	v, ok := m.gauges[name]
 	return v, ok
 }
 
 func (m *StructMem) GetCounter(name string) (int64, bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
 	v, ok := m.counters[name]
 	return v, ok
 }
-
+//теперь перед тем как отдать - делаем копию
 func (m *StructMem) GetAllGauges() map[string]float64 {
-	return m.gauges
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	mapCopy := make(map[string]float64, len(m.gauges))
+	maps.Copy(mapCopy, m.gauges)
+	return mapCopy
 }
 
+//теперь перед тем как отдать - делаем копию
 func (m *StructMem) GetAllCounters() map[string]int64 {
-	return m.counters
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	mapCopy := make(map[string]int64, len(m.counters))
+	maps.Copy(mapCopy, m.counters)
+	return mapCopy
 }
